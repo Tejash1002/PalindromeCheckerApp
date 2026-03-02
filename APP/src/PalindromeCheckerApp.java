6a28776671ab4e63900bf3c4aecf4efc522d1b86
Tejash1002 <tejash.burle@gmail.com>
2026-03-02 14:48:57 +0530

    added UC2

---
+++ APP/src/PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+
+        String word = "madam";
+
+
+        String reversedWord = "";
+
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord = reversedWord + word.charAt(i);
+        }
+
+        if (word.equals(reversedWord)) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
+
+    }
+}
